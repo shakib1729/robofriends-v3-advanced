614db89a740d1bb405cbdc1cad824c857b427863
shakib1729 <39847281+shakib1729@users.noreply.github.com>
2018-06-10 21:07:11 +0530

    robofriends v3 advanced

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Card = ({ name , email, id }) => {  //const { name , email, id } =props; we are destructuring the props directly inside the brackets
+  
+	
+	return(
+		<div className=' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
+		<div>
+			<h2> {name}</h2>
+			<p>{email}</p>
+
+		</div>
+		</div>
+
+		)
+}
+
+export default Card;
